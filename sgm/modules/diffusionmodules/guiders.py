52574ab55e92838f7d99aca35b210bf69d7610b1
antonibigata <antonibigata@yahoo.es>
2024-04-03 17:22:42 +0100

    Revert "trying to find pb with main"
    This reverts commit 65cd3b9e6416b21c91717eb24cc0153a054caefc.

--- sgm/modules/diffusionmodules/guiders.py
+++ sgm/modules/diffusionmodules/guiders.py
@@ -15,7 +15,9 @@ class Guider(ABC):
     def __call__(self, x: torch.Tensor, sigma: float) -> torch.Tensor:
         pass
 
-    def prepare_inputs(self, x: torch.Tensor, s: float, c: Dict, uc: Dict) -> Tuple[torch.Tensor, float, Dict]:
+    def prepare_inputs(
+        self, x: torch.Tensor, s: float, c: Dict, uc: Dict
+    ) -> Tuple[torch.Tensor, float, Dict]:
         pass
 
 
@@ -41,14 +43,12 @@ class VanillaCFG(Guider):
 
 
 class IdentityGuider(Guider):
-    def __init__(self, *args, **kwargs):
-        # self.num_frames = num_frames
-        pass
-
     def __call__(self, x: torch.Tensor, sigma: float) -> torch.Tensor:
         return x
 
-    def prepare_inputs(self, x: torch.Tensor, s: float, c: Dict, uc: Dict) -> Tuple[torch.Tensor, float, Dict]:
+    def prepare_inputs(
+        self, x: torch.Tensor, s: float, c: Dict, uc: Dict
+    ) -> Tuple[torch.Tensor, float, Dict]:
         c_out = dict()
 
         for k in c:
