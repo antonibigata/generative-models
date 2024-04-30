5e1569eb00a165b6ec873e3ff491f209e9d7eaea
antonibigata <antonibigata@yahoo.es>
2024-04-30 11:24:06 +0100

    choose ucg from command line

--- srun_rodrigo_no_emb.sh
+++ srun_rodrigo_no_emb.sh
@@ -23,4 +23,5 @@ srun python main.py --base configs/example_training/svd_interpolation_no_emb.yam
     model.params.network_config.params.audio_cond_method=to_time_emb \
     data.params.train.datapipeline.audio_emb_type=whisper \
     model.params.network_config.params.audio_dim=1280 \
-    model.ckpt_path=/data/home/rs2517/code/generative-models/logs/2024-04-26T18-26-41_example_training-svd_interpolation_no_emb/checkpoints/last.ckpt/checkpoint/mp_rank_00_model_states.pt \
+    model.params.ckpt_path=/data/home/rs2517/code/generative-models/logs/2024-04-26T18-26-41_example_training-svd_interpolation_no_emb/checkpoints/last.ckpt/checkpoint/mp_rank_00_model_states.pt \
+    'model.params.remove_keys_from_weights=[]'
